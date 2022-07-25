7782839845014c088afe285475ce7c3e1658d431
Sandoche Adittane <sandoche@protonmail.com>
2022-07-25 11:34:43 +0100

    Adding command to deploy

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # NEAR Javascript Enclave
 
 - Playing with the NEAR JS SDK followin this tutoria: https://docs.near.org/develop/enclave
+
+## Guide
+
+```sh
+# build
+npm run build
+
+# deploy
+npm run deploy
+```
--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
 import { NearContract, NearBindgen, call, view, near } from "near-sdk-js";
+
+// Define the default message
+const DEFAULT_MESSAGE = "Hello";
+
+@NearBindgen
+class StatusMessage extends NearContract {
+  // Define the constructor, which initializes the contract with a default message
+  constructor() {
+    // Used to give access to methods and properties of the parent or sibling class
+    super();
+    // Default the status records to
+    this.message = DEFAULT_MESSAGE;
+  }
+
+  // Public method - returns the greeting saved, defaulting to DEFAULT_MESSAGE
+  @view
+  get_greeting() {
+    env.log(`current greeting is ${this.message}`);
+    return this.message;
+  }
+
+  // Public method - accepts a greeting, such as "howdy", and records it
+  @call
+  set_greeting(message) {
+    let account_id = near.signerAccountId();
+    env.log(`Saving greeting ${message}`);
+    this.message = message;
+  }
+}
